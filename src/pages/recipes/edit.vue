8c5795e478bb0f4f4c69200b20723cefbdb5ac8e
aleigood <aleigood@gmail.com>
2025-12-15 00:06:11 +0800

    1

--- src/pages/recipes/edit.vue
+++ src/pages/recipes/edit.vue
@@ -92,7 +92,7 @@
 										</view>
 									</view>
 
-									<FormItem label="面种中面粉的比例 (%)">
+									<FormItem label="面粉的比例 (%)">
 										<input
 											class="input-field"
 											type="digit"
@@ -384,7 +384,7 @@
 					</view>
 				</picker>
 			</FormItem>
-			<FormItem label="面种中面粉的比例 (%)">
+			<FormItem label="面种中面粉占面粉总量的比例 (%)">
 				<input class="input-field" type="digit" v-model="preDoughFlourRatio" placeholder="例如：20" />
 			</FormItem>
 			<view class="modal-actions">
