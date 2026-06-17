8a5b3828bf7eb20facadb1c8faa77b0c274d68a9
aleigood <aleigood@gmail.com>
2026-06-17 11:06:15 +0800

    解决弹出窗体闪烁问题

--- src/components/AppModal.vue
+++ src/components/AppModal.vue
@@ -112,7 +112,9 @@ defineExpose({
 	left: 0;
 	right: 0;
 	bottom: 0;
-	background-color: rgba(0, 0, 0, 0.5);
+	background-color: rgba(0, 0, 0, 0.28);
+	-webkit-backdrop-filter: blur(4px);
+	backdrop-filter: blur(4px);
 	display: flex;
 	justify-content: center;
 	align-items: center;
